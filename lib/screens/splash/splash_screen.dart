d836fad09948109053c49d3db91a34072b94d93b
vyshnav.sasidharan <sasip6028@gmail.com>
2022-12-15 01:05:03 +0530

    added splash screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Image.asset("assets/images/app_splash_logo.png",
+        height: 200,
+        width: 200,),
+      ),
+    );
+  }
+}
