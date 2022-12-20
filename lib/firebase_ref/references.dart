221446f5b399b04ff7d2e53daea785628716796c
vyshnav.sasidharan <sasip6028@gmail.com>
2022-12-20 16:17:03 +0530

    added google sign in package

--- lib/firebase_ref/references.dart
+++ lib/firebase_ref/references.dart
@@ -4,6 +4,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 final firestore = FirebaseFirestore.instance;
 
 final questionPaperRef = firestore.collection("questionPapers");
+final userRef = firestore.collection('users');
 
 DocumentReference questionRef({required String paperId, required String questionId}) =>
     questionPaperRef.doc(paperId).collection("questions").doc(questionId);
